2fd08c18f4811a3911309a27d30585c370a7e222
KorobskoyRoman <shopone023@gmail.com>
2022-10-07 00:11:11 +0300

    Configured QR code frame

--- QRScanner/Modules/Scanner/View/CodeFrameView.swift
+++ QRScanner/Modules/Scanner/View/CodeFrameView.swift
@@ -5,4 +5,21 @@
 //  Created by Roman Korobskoy on 06.10.2022.
 //
 
-import Foundation
+import UIKit
+
+final class CodeFrameView: UIView {
+    init() {
+        super.init(frame: .zero)
+        setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupView() {
+        self.layer.borderColor = UIColor.white.cgColor
+        self.layer.borderWidth = 3
+        self.layer.cornerRadius = self.frame.height / 10
+    }
+}
