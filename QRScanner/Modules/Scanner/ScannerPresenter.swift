ecca45160d2133b2fb5490e25a88ce5be8663000
KorobskoyRoman <shopone023@gmail.com>
2022-10-08 18:08:20 +0300

    added viewDidLoad methods

--- QRScanner/Modules/Scanner/ScannerPresenter.swift
+++ QRScanner/Modules/Scanner/ScannerPresenter.swift
@@ -5,16 +5,23 @@
 //  Created by Roman Korobskoy on 06.10.2022.
 //
 
-import AVFoundation // зависимость
-import UIKit // зависимость от UIKit - плохо
+import AVFoundation
 
 protocol ScannerPresenterType {
+    func viewDidLoad()
     func setupCamera(captureSession: AVCaptureSession)
     func push(url: String)
 }
 
 final class ScannerPresenter: ScannerPresenterType {
     weak var coordinator: AppCoordinator?
+    weak var view: ScannerViewController?
+
+    func viewDidLoad() {
+        view?.setupCamera()
+        view?.setupOutputs()
+        view?.setupVideoPreview()
+    }
 
     func setupCamera(captureSession: AVCaptureSession) {
         guard let captureDevice = AVCaptureDevice.default(for: .video) else {
--- QRScanner/Modules/Scanner/View/ScannerViewController.swift
+++ QRScanner/Modules/Scanner/View/ScannerViewController.swift
@@ -9,7 +9,13 @@ import UIKit
 import AVFoundation
 import WebKit
 
-final class ScannerViewController: UIViewController {
+protocol ScannerView {
+    func setupCamera()
+    func setupOutputs()
+    func setupVideoPreview()
+}
+
+final class ScannerViewController: UIViewController, ScannerView {
     private var presenter: ScannerPresenterType
     private var captureSession = AVCaptureSession()
     private var videoPreviewLayer: AVCaptureVideoPreviewLayer?
@@ -27,16 +33,14 @@ final class ScannerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
-        setupCamera()
-        setupOutputs()
-        setupVideoPreview()
+        presenter.viewDidLoad()
     }
 
-    private func setupCamera() {
+    func setupCamera() {
         presenter.setupCamera(captureSession: captureSession)
     }
 
-    private func setupOutputs() {
+    func setupOutputs() {
         let captureMetadataOutput = AVCaptureMetadataOutput()
         captureSession.addOutput(captureMetadataOutput)
         captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
@@ -44,7 +48,7 @@ final class ScannerViewController: UIViewController {
         captureMetadataOutput.metadataObjectTypes = captureMetadataOutput.availableMetadataObjectTypes
     }
 
-    private func setupVideoPreview() {
+    func setupVideoPreview() {
         videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
         videoPreviewLayer?.frame = view.layer.bounds
